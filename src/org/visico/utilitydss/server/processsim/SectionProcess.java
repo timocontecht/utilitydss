26a5d0e37820296231e50f5850d94a20edaf4786
Timo <timo@timo.adsl.utwente.nl>
2012-11-14 15:41:32 +0100

    parent class SectionProcess

--- src/org/visico/utilitydss/server/processsim/SectionProcess.java
+++ src/org/visico/utilitydss/server/processsim/SectionProcess.java
@@ -19,27 +19,12 @@ public class SectionProcess extends SimProcess
 	@Override
 	public void lifeCycle() {
 		
-		   myModel.crews.provide(1);
-		   TimeInstant start = myModel.presentTime();
-		   start = myModel.presentTime();
-		   hold (new TimeSpan((getDuration()), TimeUnit.HOURS)); 
-		   ActivityMessage msg_1 = new ActivityMessage(myModel, this, start, "Remove Stones Section", myModel.presentTime());
-		   sendMessage(msg_1);
-		   myModel.crews.takeBack(1);
-		   sendTraceNote("Activity: " + getName() + " Breaking Start: " + start.toString() + 
-				   " End: " + myModel.presentTime().toString());
+		  
 		
 		
 	}
 	
-	public int getDuration() {
-		return duration;
-	}
-
-	public void setDuration(int duration) {
-		this.duration = duration;
-	}
-
+	
 	protected UtilitySimulation myModel;
-	protected int duration = 5; 
+	
 }
